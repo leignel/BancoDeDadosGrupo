9ccb32ffd6c2ec6c5ab22b686ae0466fb8a5d5c5
philippusv <felipebalbinoribeiro@gmail.com>
2023-08-22 21:55:40 -0300

    BD atualização 1

---
+++ trabalhoFinalBD.sql
@@ -0,0 +1,151 @@
+create database ofertas_express;
+
+--Grupo 2: Projeto final
+
+--Alunos:
+--Rian Fernandes
+--Cristian Leignel
+--Felipe Balbino Ribeiro
+--Igor Araujo
+--Eduardo Carneiro
+
+
+--Inserção de dados:
+
+--1) Usuario:
+create table usuario(
+	usuario_cd_id serial,
+	usuario_tx_nome varchar(50) not null,
+	usuario_tx_login varchar(50) not null,
+	usuario_tx_cpf varchar(14) not null,
+	usuario_tx_nascimento date not null,
+	fk_end_cd_id integer,
+	fk_contato_cd_id integer,
+	primary key (usuario_cd_id),
+	foreign key(fk_end_cd_id) references endereco (end_cd_id),
+	foreign key(fk_contato_cd_id) references contato (contato_cd_id)
+);
+
+--2) Endereço:
+create table endereco(
+	end_cd_id serial,
+	end_tx_rua varchar(100) not null,
+	end_int_num integer not null,
+	end_tx_complemento varchar(20) not null,
+	end_tx_bairro varchar(30) not null,
+	end_tx_cidade varchar(30) not null,
+	end_tx_estado varchar(30) not null,
+	end_tx_cep varchar(9) not null,
+	primary key(end_cd_id)
+);
+
+--3) Contato:
+create table contato(
+	contato_cd_id serial,
+	cont_tx_telfix varchar(15) not null,
+	end_tx_celular varchar(16) not null,
+	end_tx_email varchar(100) not null,
+	primary key (contato_cd_id)
+);
+
+--4) Categoria:
+create table categoria(
+	cate_cd_id serial,
+	cate_tx_nome varchar(30) not null,
+	cate_tx_descricao varchar(200) not null,
+	primary key(cate_cd_id)
+);
+
+--5) Produto:
+create table produto(
+	prod_cd_id serial,
+	prod_tx_nome varchar(30) not null,
+	prod_tx_descricao varchar(200) not null,
+	prod_int_qntestoque integer not null,
+	prod_dt_datafabri date not null,
+	prod_nm_valoruni decimal not null,
+	fk_usuario_id integer,
+	fk_cate_id integer,
+	primary key (prod_cd_id),
+	foreign key (fk_usuario_id) references usuario(usuario_cd_id),
+	foreign key (fk_cate_id) references categoria(cate_cd_id)
+);
+
+--6) pedidoProduto:
+create table pedidoproduto(
+	pedprod_cd_id serial,
+	pedprod_dt_data date not null,
+	pedprod_int_qnt integer not null,
+	fk_prod_cd_id integer,
+	primary key(pedprod_cd_id),
+	foreign key(fk_prod_cd_id) references produto(prod_cd_id)
+);
+
+--7) Pedido
+create table pedido (
+	pedido_cd_id serial,
+	pedido_dt_data date not null,
+	fk_pedprod_cd_id integer,
+	fk_usuario_cd_id integer,
+	primary key(pedido_cd_id),
+	foreign key(fk_pedprod_cd_id) references pedidoproduto(pedprod_cd_id),
+	foreign key(fk_usuario_cd_id) references usuario(usuario_cd_id)
+);
+
+--Inserção de dados das tabelas
+
+--1)Dados contato:
+insert into contato(cont_tx_telfix, end_tx_celular, end_tx_email) values 
+('(11)1234-5678','(11)98765-4321','rian@gmail.com'),
+('(21)9876-5432','(21)97654-3210','cris@hotmail.com'),
+('(31)5555-1234','(31)96543-2109','igor@yahoo.com'),
+('(41)8888-4327','(41)95432-1096','edu@outlook.com'),
+('(51)7777-8765','(51)94321-0984','felps@gmail.com');
+
+--2)Endereço:
+insert into endereco(end_tx_rua, end_int_num, end_tx_complemento, end_tx_bairro, end_tx_cidade, end_tx_estado, end_tx_cep) values
+('Rua das Flores',260,'Casa florida','Jardim Primavera','São Paulo','São Paulo','54321-987'),
+('Avenida Central',750,'Condominio','Centro','Rio de Janeiro','Niterói','98765-432'),
+('Rua da Amizade',777,'Apartamento','Amizade','Minas Gerais','Uberlândia','22222-333'),
+('Rua dos Sonhos',825,'Rua sem saida','Bairro dos Sonhos','Paraná','Curitiba','55555-444'),
+('Avenida da Liberdade',333,'Do lado da prisão','Neylor','Rio Grande do Sul','Porto Alegre','78965-321');
+
+--3)Categoria:
+insert into categoria(cate_tx_nome,cate_tx_descricao) values 
+('Eletrônicos','Os melhores aparelhos da atualizada'),
+('Brinquedo','Variedades para todas as crianças'),
+('Roupas','Aqui voce pode renovar seu guarda roupa e estar sempre na moda'),
+('Livros','Livros de todos os gêneros e idades'),
+('Cozinha','Os produtos para deixar sua cozinha pronta para aquele almoço de domingo');
+
+--4)Usuário:
+insert into usuario(usuario_tx_nome,usuario_tx_login,usuario_tx_cpf,usuario_tx_nascimento,fk_end_cd_id,fk_contato_cd_id) values
+('Rian','rianzingamer08','123.456.789-01','23/03/1998',2,1),
+('Cristian','leignelsonpedreiro','987.654.321-09','23/09/1995',1,2),
+('Felipe','felipeDoTremBala','562.555.545-85','13/09/2001',4,3),
+('Eduardo','eduardoEmonica','819.239.008-77','20/11/1990',5,4),
+('Igor','igorbatima','777.777.777-77','21/03/2005',3,5);
+
+--5)Produto:
+insert into produto(prod_tx_nome, prod_tx_descricao,prod_int_qntestoque,prod_dt_datafabri, prod_nm_valoruni,fk_usuario_id,fk_cate_id)values
+('Iphone 25','Preto 256 gb', 5,'25/08/2020',12000.00,5,1),
+('Forno a jato','Possui 10 bocas e um componente de foguete', 10,'17/02/2021',20000.00,4,5),
+('Nerf minigun','Azul com dois mil cartuchos', 30,'25/12/2022',150.00,3,2),
+('Casaco da nike de tricô','Azul marinha', 100,'21/08/2023',750.00,2,3),
+('Grimório perdido','Todas as magias, literalmente todas', 1,'20/07/1320',500000.00,1,4);
+
+--6)Pedido-Produto:
+insert into pedidoproduto(pedprod_dt_data, pedprod_int_qnt,fk_prod_cd_id) values
+('10/08/2023',4,1),
+('12/08/2023',2,3),
+('13/08/2023',1,5),
+('14/08/2023',8,2),
+('22/08/2023',1,4);
+
+--7)Pedido:
+insert into pedido(pedido_dt_data,fk_pedprod_cd_id,fk_usuario_cd_id) values
+('09/08/2023',1,5),
+('09/08/2023',3,3),
+('09/08/2023',5,1),
+('09/08/2023',2,4),
+('09/08/2023',4,2);
